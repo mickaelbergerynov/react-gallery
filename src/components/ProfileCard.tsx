296a750eaeba4d7b18050fcee6f5d52ee8ce964d
BERGER Mickael <mberger@iocean.fr>
2024-04-24 17:46:07 +0200

    Implement local state

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,15 +1,16 @@
 import userIcon from '../resources/user_icon.jpg'
 import refreshIcon from '../resources/refresh_icon.png'
-import { useContext } from 'react';
-import { IUserContext, UserContext } from '../App';
+import { useState } from 'react';
+import getRandomName from '../utils/getRandomName';
 function ProfileCard() {
-    const { username, onRefreshClick } = useContext<IUserContext>(UserContext);
+    const [username, setUsername] = useState(getRandomName());
     return <div className="profile-card">
         <img src={userIcon} className="user-logo" alt="logo" />
         <p>
             <code>{username}</code>
         </p>
-        <img src={refreshIcon} className="user-refresh-icon" alt="refresh username" onClick={() => onRefreshClick()} />
+        <img src={refreshIcon} className="user-refresh-icon" alt="refresh username" 
+            onClick={() => setUsername(getRandomName())} />
     </div>
 }
 
